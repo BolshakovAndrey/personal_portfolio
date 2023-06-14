be1f387bb944fc8336d121da8bc4ff45bfff6cd5
Andrey Bolshakov <abolshakovy@gmail.com>
2023-06-14 22:57:10 +0500

    feature(about) about info done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React from "react";
+import Info from "../../components/Info";
+import {FaDownload} from "react-icons/fa";
+import CV from "../../assets/AndreBolshakov-CV.pdf"
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <main className="section container">
+            <section className="about"></section>
+            <h2 className="section__title">
+                About <span>Me</span>
+            </h2>
+
+            <div className="about__container grid">
+                <div className="about__info">
+                    <h3 className="section__subtitle">
+                        Personal Information
+                    </h3>
+
+                    <ul className="info__list grid">
+                        <Info/>
+                    </ul>
+
+                    <a href={CV} className="button">
+                        Download CV{" "}
+                        <span className="button__icon">
+                        <FaDownload/>
+                    </span>
+                    </a>
+                </div>
+            </div>
+        </main>
     );
 };
 
